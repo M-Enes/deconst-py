7e838e66491edbf5ec45db54bec035c296f1eaaa
DevelopMEnes <karaca.1453.27@gmail.com>
2021-05-01 21:36:13 +0300

    Initial commit

---
+++ deconstpy/sample.py
@@ -0,0 +1,5 @@
+const  = 'sample data' 
+print('sample string 2')
+if 'Sample string' == 'Sample string':
+    print('Sample string')
+# sample comment
---
+++ sample.py
@@ -0,0 +1,6 @@
+CONSTANT, CONSTANT2 = 'Sample string', 'sample string 2'
+const, CONST = 'sample data', 'sample 2'
+print(CONSTANT)
+if CONSTANT == 'Sample string':
+    print(CONSTANT)
+# sample comment
